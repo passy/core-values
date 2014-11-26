aff4bfebcb954df876fa2caa88b822fe54329365
Pascal Hartig <passy@twitter.com>
2014-11-26 23:30:49 +0000

    Render something on the screen, basic step loop

--- CoreValues.elm
+++ CoreValues.elm
@@ -1,17 +1,50 @@
 module CoreValues where
 
-main : Element
-main = flow down sentences
+import Html (..)
+import Html.Tags as H
+import Html.Attributes as H
+import Window
 
-sentences : [Element]
-sentences =
-    let msg = "\nThe quick brown fox jumps over the lazy dog.\n"
-        sentence tfs = leftAligned (typeface tfs (toText msg))
-    in
-        map sentence
-        [ ["times new roman"]
-        , ["helvetica", "sans-serif"]
-        , ["georgia", "serif"]
-        , ["trebuchet ms", "sans-serif"]
-        , ["inconsolata", "courier new", "monospace"]
-        ]
+import Graphics.Input as Input
+
+type Color = String
+
+type State =
+    { text : String
+    , bgColor : Color
+    , textColor : Color
+    }
+
+data Action = NoOp
+
+defaultState : State
+defaultState =
+    { text = "Innovate through experimentation."
+    , textColor = "#FFFFFF"
+    , bgColor = "#55ACEE"
+    }
+
+main : Signal Element
+main = lift2 scene state Window.dimensions
+
+-- Stub so far, obviously.
+step : Action -> State -> State
+step action state =
+    case action of
+        NoOp -> state
+
+scene : State -> (Int, Int) -> Element
+scene state (w, h) =
+    container w h midTop (toElement 550 h (view state))
+
+actions : Input.Input Action
+actions = Input.input NoOp
+
+state : Signal State
+state = foldp step defaultState actions.signal
+
+view : State -> Html
+view state =
+    H.div
+        [ H.class "main" ]
+        [ H.h1 [] [ text state.text ] ]
